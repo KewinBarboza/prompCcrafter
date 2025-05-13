4e4ea43a1b9586429ed4e438df153f64056622f0
kewinBarboza <kewin.barboza@sybven.com>
2025-05-13 16:25:36 -0400

    first commit

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,68 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: "",
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: "",
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: "",
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: "",
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: "",
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar variant="floating">
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      {/* <item.icon /> */}
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
