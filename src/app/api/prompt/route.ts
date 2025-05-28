2b83a2329e7ea250b0df2489f5d7211edde8f3b8
kewinBarboza <kewin.barboza@sybven.com>
2025-05-28 17:53:43 -0400

    fix: improve error handling and response structure in POST request

--- src/app/api/prompt/route.ts
+++ src/app/api/prompt/route.ts
@@ -7,23 +7,30 @@ export const maxDuration = 30
 
 export async function POST(req: Request) {
   const prompt = await req.text()
+
   if (!prompt) {
     return new Response('Prompt is required', { status: 400 })
   }
 
-  const result = generateObject({
-    model: google('gemini-2.0-flash'),
-    schema: z.object({
-      prompt: z.array(
-        z.object({
-          name: z.string(),
-          prompt: z.string(),
-        })
-      )
-    }),
-    prompt: prompt,
-    output: 'object'
-  })
+  try {
+    const result = generateObject({
+      model: google('gemini-2.0-flash'),
+      schema: z.object({
+        prompt: z.array(
+          z.object({
+            name: z.string(),
+            prompt: z.string(),
+          })
+        )
+      }),
+      system: 'Eres un experto en prompts para IA. Tu tarea es generar prompts de alta calidad que ayuden a los usuarios a obtener mejores resultados de los modelos de IA. los prompts deben estar en español, ser claros y directos, y seguir las mejores prácticas de diseño para que sean efectivos y fáciles de entender a la ia lo que tiene que hacer.',
+      prompt: prompt,
+      output: 'object'
+    })
 
-  return (await result).toJsonResponse()
+    return (await result).toJsonResponse()
+  } catch (error) {
+    console.error('Error processing request:', error)
+    return new Response('Internal Server Error', { status: 500 })
+  }
 }
