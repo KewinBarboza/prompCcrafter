e596d6c267730865a5ce50c88c25ba806514cbe6
kewinBarboza <kewin.barboza@sybven.com>
2025-05-28 15:00:26 -0400

    feat: add handleRadioToTextarea function to synchronize radio input with corresponding textarea

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function handleRadioToTextarea(e: React.ChangeEvent<HTMLInputElement>) {
+  const { name, value } = e.target
+  const textarea = document.querySelector(`textarea[name='${name}']`) as HTMLTextAreaElement
+
+  if (textarea) {
+    textarea.value = value
+    textarea.dispatchEvent(new Event('input', { bubbles: true }))
+  }
+}
